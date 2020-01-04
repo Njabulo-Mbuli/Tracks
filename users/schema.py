fcb12e865088237aac5a08826f9bf8a0dbda28f6
Njabulo-Mbuli <admin@njabulombuli.co.zagit config --global user.email admin@njabulombuli.co.za>
2020-01-04 11:00:55 +0200

    Completed the backend

--- users/schema.py
+++ users/schema.py
@@ -9,6 +9,15 @@ class UserType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     user = graphene.Field(UserType,id=graphene.Int(required=True))
+    me = graphene.Field(UserType)
+
+    def resolve_me(self,info):
+        user = info.context.user
+
+        if user.is_anonymous:
+            raise GraphQLError('Not logged in!')
+
+        return user
 
     def resolve_user(self,info,id):
         return get_user_model().objects.get(id=id)
