a572e86af68b3440cdda19b4b4cedc05f4316963
Njabulo-Mbuli <hopewell.mbuli@gmail.com>
2020-03-06 21:37:08 +0200

    Small change

--- client/src/components/Shared/Footer.js
+++ client/src/components/Shared/Footer.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import withStyles from "@material-ui/core/styles/withStyles";
 
 
